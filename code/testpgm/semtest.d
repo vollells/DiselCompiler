8d54050d5d131506b4a4778b6e29108065a68478
Edvard Thörnros <edvard.thornros@gmail.com>
2021-11-10 10:17:18 +0100

    Add test for casting

--- code/testpgm/semtest.d
+++ code/testpgm/semtest.d
@@ -1,18 +1,22 @@
 program semtest1;
 
+var
+    a: real;
+
 procedure down(n : integer);
 begin
 end;
 
 begin
+  a := 2;
   if 0 then
-     down();
+     down(1);
   elsif 1>0 then
-     down();
+     down(1);
   elsif 1>0 then
-     down();
+     down(1);
   else
-     down();
+     down(1);
   end;
 
 end.
